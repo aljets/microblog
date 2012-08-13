ca948d9f18830cc907367c72e19a19a2f3eb5d9e
Adam <adam@git>
2012-08-13 11:28:36 -0700

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) { "microblog" }
-
   describe "Home page" do
 
     it "should have the h1 'microblog'" do
@@ -11,11 +9,17 @@ describe "StaticPages" do
       page.should have_selector('h1', :text => 'microblog')
     end
 
-    it "should have the right title 'home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                                :text=> "#{base_title} | home")
+                                :text=> "microblog")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => "home")
+    end
+
   end
   
   describe "Help page" do
@@ -25,9 +29,9 @@ describe "StaticPages" do
       page.should have_selector('h1', :text => 'Help')
     end
 
-    it "should have the right title 'help'" do
+    it "should have a custom title 'help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "#{base_title} | help")
+      page.should have_selector('title', :text => "help")
     end
 
   end
@@ -39,9 +43,9 @@ describe "StaticPages" do
       page.should have_selector('h1', :text => 'About Us')
     end
 
-    it "should have the right title 'about us'" do
+    it "should have a custom title 'about us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "#{base_title} | about us")
+      page.should have_selector('title', :text => "about us")
     end
 
   end
