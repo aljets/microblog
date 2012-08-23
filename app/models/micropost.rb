1786af0b42eecf521ffe9899db353684d1f8a0e9
Adam <adam@git>
2012-08-23 15:00:22 -0700

    Add followed users to feed; complete microblog functionality

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -12,4 +12,13 @@ class Micropost < ActiveRecord::Base
 
   # Sets user.microposts default order to descending by daate
   default_scope order: 'microposts.created_at DESC'
+
+  # Returns microposts from the users being followed by the given user and the 
+  # user's own microposts
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
+          user_id: user)
+  end
 end
